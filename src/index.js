699f6e636376468c0581c2e2f329da9ae9837d2b
BrendaG06 <projetomedays@gmail.com>
2024-11-21 01:20:45 -0300

    commit api2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({ origin: 'https://front-ten-alpha.vercel.app/' }));
+app.use(cors({ origin: 'https://front-ten-alpha.vercel.app' }));
 app.get('/', (req, res) => { res.send('API RODANDO');});
 app.use('/api', consultaRoutes);
 app.use('/usuarios', rotasUsuarios);
